2784206a384b059f3fbc25cb7a1cb5cfad2f780e
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-03 19:29:05 +0200

    remembering form data and css improved

--- src/main/java/pl/codeschool/controller/AdminEditExercise.java
+++ src/main/java/pl/codeschool/controller/AdminEditExercise.java
@@ -24,6 +24,7 @@ public class AdminEditExercise extends HttpServlet {
 
         String title = request.getParameter("title");
         String description = request.getParameter("description");
+        String paramExerciseId = request.getParameter("exerciseId");
 
         Exercise exercise = (Exercise) request.getSession().getAttribute("exercise");
         int exerciseId = 0;
@@ -31,28 +32,17 @@ public class AdminEditExercise extends HttpServlet {
             exerciseId = exercise.getId();
         }
 
-        if (title != null && !"".equals(title) && description != null && !"".equals(description)) {
+        validateParams(title, description, request, response);
 
+        if (!"".equals(title) && !"".equals(description)) {
             Exercise updatedExercise = new Exercise(exerciseId, title, description);
             ExerciseDao.update(updatedExercise);
 
-            //NIE DZIAŁA -SPRAWDZ
             request.setAttribute("isUpdated", true);
+            request.setAttribute("exerciseId", paramExerciseId);
+
             request.getRequestDispatcher("/WEB-INF/admin-edit-exercise.jsp")
                     .forward(request, response);
-        }  else {
-            String isBlank = "this field cannot be empty";
-            if ("".equals(title) && "".equals(description)) {
-                request.setAttribute("blankTitle", isBlank);
-                request.setAttribute("blankDescription", isBlank);
-                doGet(request, response);
-            } else if ("".equals(title)) {
-                request.setAttribute("blankTitle", isBlank);
-                doGet(request, response);
-            } else {
-                request.setAttribute("blankDescription", isBlank);
-                doGet(request, response);
-            }
         }
     }
 
@@ -62,16 +52,64 @@ public class AdminEditExercise extends HttpServlet {
 
         String paramExerciseId = request.getParameter("exerciseId");
 
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+
         if (paramExerciseId != null && !"".equals(paramExerciseId)) {
             try {
                 int exerciseId = Integer.parseInt(paramExerciseId);
                 Exercise founded = ExerciseDao.read(exerciseId);
                 request.getSession().setAttribute("exercise", founded);
+                fillTheFormWithLatestData(founded.getTitle(), founded.getDescription(), request);        fillTheFormWithLatestData(title, description, request);
+                fillTheFormWithLatestData(title, description, request);
             } catch (NumberFormatException e) {
                 e.printStackTrace();
             }
         }
         request.getRequestDispatcher("/WEB-INF/admin-edit-exercise.jsp")
                 .forward(request, response);
+    }
+
+    private void validateParams(String title, String description, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final String EMPTY_FIELD = "this field cannot be empty";
+        final String CHARS_CAPACITY_EXCEEDED = "allowed number of characters exceeded";
+
+        int titleLength = title.length();
+        int descriptionLength = description.length();
+
+        if ("".equals(title) && "".equals(description)) {
+            request.setAttribute("blankTitle", EMPTY_FIELD);
+            request.setAttribute("blankDescription", EMPTY_FIELD);
+            doGet(request, response);
+        } else if ("".equals(title)) {
+            request.setAttribute("blankTitle", EMPTY_FIELD);
+            doGet(request, response);
+        } else if ("".equals(description)) {
+            request.setAttribute("blankDescription", EMPTY_FIELD);
+            doGet(request, response);
+        } else if (titleLength > 256 && descriptionLength > 65535) {
+            request.setAttribute("tooManyCharsTitle", CHARS_CAPACITY_EXCEEDED);
+            request.setAttribute("tooManyCharsDesc", CHARS_CAPACITY_EXCEEDED);
+            doGet(request, response);
+        } else if (titleLength > 256) {
+            request.setAttribute("tooManyCharsTitle", CHARS_CAPACITY_EXCEEDED);
+            doGet(request, response);
+        } else if (descriptionLength > 65535) {
+            request.setAttribute("tooManyCharsDesc", CHARS_CAPACITY_EXCEEDED);
+            doGet(request, response);
+        }
+    }
+
+    private void fillTheFormWithLatestData(String title, String description, HttpServletRequest request) {
+        if (title != null && description != null) {
+            if (!"".equals(title) && !"".equals(description)) {
+                request.setAttribute("titleVal", title);
+                request.setAttribute("descriptionVal", description);
+            } else if (!"".equals(title)) {
+                request.setAttribute("titleVal", title);
+            } else if (!"".equals(description)) {
+                request.setAttribute("descriptionVal", description);
+            }
+        }
     }
 }
--- src/main/webapp/WEB-INF/admin-edit-exercise.jsp
+++ src/main/webapp/WEB-INF/admin-edit-exercise.jsp
@@ -1,7 +1,9 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <c:set var="contextPath" value="${pageContext.request.contextPath}"/>
-
+<style>
+    <%@ include file="/resources/css/styles.css"%>
+</style>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,40 +11,53 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Admin edit exercise</title>
-    <link href='<c:url value="/css/style.css"/>' rel="stylesheet" type="text/css">
+    <link href='<c:url value="/resources/css/styles.css"/>' rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
     <%@ include file="/WEB-INF/fragment/navbar.jsp" %>
 
-    <div class="container-add-exc">
-        <h1>Edit exercise ${exercise.title}</h1>
+    <div class="main-container-exc">
+        <div class="container-exc">
+            <h2>Edit exercise ${exercise.title}</h2>
 
-        <div class="content-add-exc">
-    <%--    //DODAJ FUNKCJONALNOŚĆ ZAPISYWANIA SIĘ POPRZEDNIEGO TYTUŁU I OPISU--%>
-            <form action="${contextPath}/adminEditExercise" method="post">
-                <label>
-                    <input type="text" name="title" placeholder="Exercise name">
-                    <c:if test="${not empty blankTitle}">
-                        <br><label class="hasError"><c:out value="${blankTitle}"/></label>
-                    </c:if>
-                </label>
-                <br>
+            <div class="content-exc">
+                <form action="${contextPath}/adminEditExercise" method="post">
+                    <input type="hidden" value="${exercise.id}" name="exerciseId">
+                    <label>
+                        <input type="text" name="title" placeholder="Exercise title" value="<c:out value='${titleVal}'/>">
+                        <c:if test="${not empty blankTitle}">
+                            <label class="hasError"><c:out value="${blankTitle}"/></label>
+                        </c:if>
+                        <c:if test="${not empty tooManyCharsTitle}">
+                            <label class="hasError"><c:out value="${tooManyCharsTitle}"/></label>
+                        </c:if>
+                    </label>
 
-                <label>
-                    <input type="text" name="description" placeholder="Exercise description">
-                    <c:if test="${not empty blankDescription}">
-                        <br><label class="hasError"><c:out value="${blankDescription}"/></label>
-                    </c:if>
-                </label><br>
-                <input class="addButton" type="submit" value="Save">
-            </form>
+                    <br>
+
+                    <label>
+                        <textarea name="description" placeholder="Exercise description" rows="10"><c:out value="${descriptionVal}"/></textarea>
+                        <c:if test="${not empty blankDescription}">
+                            <label class="hasError"><c:out value="${blankDescription}"/></label>
+                        </c:if>
+                        <c:if test="${not empty tooManyCharsDesc}">
+                            <label class="hasError"><c:out value="${tooManyCharsDesc}"/></label>
+                        </c:if>
+                    </label>
+
+                    <label>
+                        <input class="addButton" type="submit" value="Save">
+                    </label>
+
+                </form>
+            </div>
         </div>
-    </div>
 
-    <div class="backButton">
-        <button class="addButton"><a class="addButton" href="${contextPath}/adminExercises">Back</a></button>
+        <div class="backButton">
+            <button class="addButton"><a class="addButton" href="${contextPath}/adminExercises">Back</a></button>
+        </div>
     </div>
 
     <c:if test="${isUpdated eq true}">
