645bc64cc9a3f0570c63fc6666d07bbd4023e1f6
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-06 15:37:54 +0200

    servelt path changed and postfix Controller added into InfoExerciseSolutionController

--- src/main/java/pl/codeschool/controller/InfoExerciseSolutionsController.java
+++ src/main/java/pl/codeschool/controller/InfoExerciseSolutionsController.java
@@ -13,12 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/infoSolutions")
-public class AdminExerciseSolutionsController extends HttpServlet {
+@WebServlet("/infoExerciseSolutions")
+public class InfoExerciseSolutionsController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
 
         String paramExerciseId = request.getParameter("exerciseId");
