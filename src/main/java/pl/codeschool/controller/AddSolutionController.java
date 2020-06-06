b4dda985bc326e389528d2929e4665f3ea2da856
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-06 15:49:59 +0200

    AddSolutionController started

---
+++ src/main/java/pl/codeschool/controller/AddSolutionController.java
@@ -0,0 +1,24 @@
+package pl.codeschool.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/addSolution")
+public class AddSolutionController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+
+    }
+}
