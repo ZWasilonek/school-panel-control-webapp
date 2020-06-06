929c8650b6357161faf2827c2bcae2e952cc2767
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-06 15:48:54 +0200

    ExercisePanelController added

---
+++ src/main/java/pl/codeschool/controller/ExercisePanelController.java
@@ -0,0 +1,31 @@
+package pl.codeschool.controller;
+
+import pl.codeschool.dao.ExerciseDao;
+import pl.codeschool.model.Exercise;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/exercisesPanel")
+public class ExercisePanelController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+
+        List<Exercise> allExercises = ExerciseDao.findAll();
+        if (allExercises != null && allExercises.size() != 0) {
+            request.setAttribute("exercises", allExercises);
+        } else {
+            request.setAttribute("exercisesNotFound", true);
+        }
+
+        request.getRequestDispatcher("WEB-INF/exercises.jsp")
+                .forward(request, response);
+    }
+
+}
