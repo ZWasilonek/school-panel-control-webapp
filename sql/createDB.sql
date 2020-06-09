3e1adabf19f2ec4bad495b6f566fc666a314be8c
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-09 16:01:04 +0200

    sql - isAdmin, Group ADMIN added and fixed others fields

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -6,24 +6,27 @@ CREATE TABLE users_groups(
      PRIMARY KEY (id)
 ) ENGINE=INNODB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-INSERT INTO users_groups (name) VALUES
-    ('Group A'),
-    ('Group B');
+INSERT INTO users_groups (id, name) VALUES
+    (1, 'Group ADMIN'),
+    (2, 'Group A'),
+    (3, 'Group B');
 
 CREATE TABLE users(
       id INT AUTO_INCREMENT,
       username VARCHAR(256),
       email VARCHAR(256) UNIQUE,
-      password VARCHAR(256),
+      password VARCHAR(256) NOT NULL,
       group_id INT NOT NULL,
+      is_admin BOOLEAN NOT NULL,
       PRIMARY KEY (id),
       FOREIGN KEY (group_id) REFERENCES users_groups(id)
 ) ENGINE=INNODB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-INSERT INTO users (username, email, group_id) VALUES
-    ('Marcin Cieszkowski', 'marcin.cieszkowski@gmail.com', 1),
-    ('Janusz Kowalski', 'jan.kowalski@wp.pl', 1),
-    ('Anna Nowak', 'anna.nowak@o2.pl', 2);
+INSERT INTO users (username, email, password, group_id, is_admin) VALUES
+    ('ADMIN', 'admin@gmail.com', 'adminpass', 1, true),
+    ('Marcin Cieszkowski', 'marcin.cieszkowski@gmail.com', 'password', 2, false),
+    ('Janusz Kowalski', 'jan.kowalski@wp.pl', 'password', 2, false),
+    ('Anna Nowak', 'anna.nowak@o2.pl', 'password', 3, false);
 
 CREATE TABLE exercises(
       id INT AUTO_INCREMENT,
@@ -49,7 +52,7 @@ CREATE TABLE solutions(
 ) ENGINE=INNODB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 INSERT INTO solutions (user_id, exercise_id, created, updated, description) VALUES
-    (1, 1, '2020-06-05', NOW() - INTERVAL 3 HOUR,
+    (2, 1, '2020-06-05', NOW() - INTERVAL 3 HOUR,
      'First we calculate a from the formula:
     h=aV3/2
 
@@ -63,10 +66,10 @@ INSERT INTO solutions (user_id, exercise_id, created, updated, description) VALU
     P=64/3*V3/4
     P=16V3/3 cm^2'
     ),
-    (1, 2, '2020-06-01', NOW() - INTERVAL 2 HOUR, 'I don''t even know how to start, I give up'),
-    (2, 1, '2020-05-20', NOW() - INTERVAL 2 DAY, 'I don''t know how to solve this exercise'),
-    (2, 2, '2020-06-03', NOW() - INTERVAL 2 DAY,
-        'A(-7;7)
+    (2, 2, '2020-06-01', NOW() - INTERVAL 2 HOUR, 'I don''t even know how to start, I give up'),
+    (3, 1, '2020-05-20', NOW() - INTERVAL 2 DAY, 'I don''t know how to solve this exercise'),
+    (3, 2, '2020-06-03', NOW() - INTERVAL 2 DAY,
+            'A(-7;7)
     B(11,1)
     *we use the formula for the middle of the episode : S=[ ( x1+x2)/2 ; (y1+y2)/2]
 
@@ -77,8 +80,8 @@ INSERT INTO solutions (user_id, exercise_id, created, updated, description) VALU
     S=-(2;4 )
 
     The origin of the coordinate system is a point P=(0;0)
-    When calculating the distance between the middle of the segment and the beginning of the coordinate system, we use the
-    formula:
+    When calculating the distance between the middle of the segment and the beginning of the coordinate
+    system, we use the formula:
 
     AB^2=(x2-x1)^2 + (y2-y1)^2
 
@@ -89,7 +92,7 @@ INSERT INTO solutions (user_id, exercise_id, created, updated, description) VALU
     AB=V20=V(4*5)
     AB=2V5'
     ),
-    (3, 1, '2020-04-28', NOW() - INTERVAL 1 DAY,
+    (4, 1, '2020-04-28', NOW() - INTERVAL 1 DAY,
      '  h=a√3/2
     h=4cm
     4=a√3/2 /·2
