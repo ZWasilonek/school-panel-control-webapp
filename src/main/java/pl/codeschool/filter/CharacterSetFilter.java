09a81bfc84004f008df579c506a8cc48dfef196c
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-10 18:39:20 +0200

    urlPatterns fixed {/*} for character encoding UTF-8

--- src/main/java/pl/codeschool/filter/CharacterSetFilter.java
+++ src/main/java/pl/codeschool/filter/CharacterSetFilter.java
@@ -1,8 +1,10 @@
 package pl.codeschool.filter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
+@WebFilter(urlPatterns = {"/*"})
 public class CharacterSetFilter implements Filter {
 
     @Override
@@ -11,8 +13,6 @@ public class CharacterSetFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain next) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
         next.doFilter(request, response);
     }
 
