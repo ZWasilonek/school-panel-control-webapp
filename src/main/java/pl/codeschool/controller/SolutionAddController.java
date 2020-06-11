451d546fc2009ecb309f644cfff1627bd39de042
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-11 17:51:17 +0200

    SolutionAddController finished

--- src/main/java/pl/codeschool/controller/SolutionAddController.java
+++ src/main/java/pl/codeschool/controller/SolutionAddController.java
@@ -3,6 +3,7 @@ package pl.codeschool.controller;
 import pl.codeschool.dao.ExerciseDao;
 import pl.codeschool.dao.SolutionDao;
 import pl.codeschool.dao.UserDao;
+import pl.codeschool.mapper.DataFiller;
 import pl.codeschool.model.Exercise;
 import pl.codeschool.model.Solution;
 import pl.codeschool.model.User;
@@ -17,8 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
-@WebServlet("/addSolution")
-public class AddSolutionController extends HttpServlet {
+@WebServlet("/add/solution")
+public class SolutionAddController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -26,7 +27,12 @@ public class AddSolutionController extends HttpServlet {
 
         String paramDescription = request.getParameter("description");
         int exerciseId = (int) request.getSession().getAttribute("exerciseId");
-        int userId = (int) request.getSession().getAttribute("userId");
+
+        Integer userId;
+        userId = (Integer) request.getSession().getAttribute("userId");
+        if (userId == null) {
+            userId = (Integer) request.getSession().getAttribute("adminId");
+        }
 
         Map<String, String> fieldNames = Map.of("description", paramDescription);
         boolean hasBlankField = BlankValidation.hasBlankErrorsAttributes(fieldNames, request);
@@ -40,9 +46,9 @@ public class AddSolutionController extends HttpServlet {
             if (foundedExercise != null && foundedUser != null) {
                 Solution newSolution = new Solution(foundedUser, foundedExercise, paramDescription);
                 SolutionDao.create(newSolution);
-                request.setAttribute("exerciseCreated", true);
+                request.setAttribute("solutionCreated", true);
             }
-            request.getRequestDispatcher("/WEB-INF/admin-add-exercise.jsp")
+            request.getRequestDispatcher("/WEB-INF/solution-add.jsp")
                     .forward(request, response);
         } else doGet(request, response);
     }
@@ -51,7 +57,46 @@ public class AddSolutionController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset=UTF-8");
 
-        request.getRequestDispatcher("/WEB-INF/solution-add.jsp").forward(request,response);
+        String paramDescription = request.getParameter("description");
+        String paramExerciseId = request.getParameter("exerciseId");
 
+        Integer userId;
+        userId = (Integer) request.getSession().getAttribute("userId");
+        if (userId == null) {
+            userId = (Integer) request.getSession().getAttribute("adminId");
+        }
+
+        Integer exerciseId = geExerciseId(request, paramExerciseId);
+
+        if (exerciseId != null && userId != null) {
+            Exercise foundedExercise = ExerciseDao.read(exerciseId);
+            User foundedUser = UserDao.read(userId);
+
+            if (foundedExercise != null && foundedUser != null) {
+                if (paramDescription != null) {
+                    DataFiller.modelAttributesFiller(
+                            Map.of("description", paramDescription), request);
+                }
+                request.getSession().setAttribute("exerciseId", foundedExercise.getId());
+                request.setAttribute("exercise", foundedExercise);
+            }
+        }
+        request.getRequestDispatcher("/WEB-INF/solution-add.jsp").forward(request,response);
     }
+
+    private Integer geExerciseId(HttpServletRequest request, String paramExerciseId) {
+        Integer exerciseId = null;
+
+        if (paramExerciseId == null || "".equals(paramExerciseId)) {
+            exerciseId = (Integer) request.getSession().getAttribute("exerciseId");
+        } else {
+            try {
+                exerciseId = Integer.parseInt(paramExerciseId);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return exerciseId;
+    }
+
 }
